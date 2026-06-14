4d3b88fe7676951f6db5344d6a82a38f6be13e9f
Manoj Dilshan <manojdilz.me@gmail.com>
2026-06-14 11:59:54 +0530

    add product

---
+++ simple-shopping-app/product/KeyBoard.java
@@ -0,0 +1,7 @@
+package product;
+
+public class KeyBoard extends Product {
+    public KeyBoard(int price) {
+        super("KeyBoard", price);
+    }
+}
---
+++ simple-shopping-app/product/Phone.java
@@ -0,0 +1,8 @@
+package product;
+
+public class Phone extends Product {
+    public Phone(int price) {
+        super("Phone", price);
+    }
+
+}
